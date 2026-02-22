b40e2324a060b7bdf57d4ee5a0b40e325bf18ee6
marcelofdocente <marcelo.f@docente.senai.br>
2026-02-22 11:55:03 -0300

    primeira versao

---
+++ BD/script-bd - Copy.sql
@@ -0,0 +1,15 @@
+create database livro_db_v2;
+use livro_db_v2;
+
+create table Usuarios
+(
+id_usuario INT primary key auto_increment,
+nome varchar(100) NOT NULL,
+sobrenome varchar(100) NOT NULL,
+email varchar(150) NOT NULL UNIQUE,
+senha varchar(255) NOT NULL
+);
+
+-- ATIVIDADE PRATICA 1
+
+-- ATIVIDADE PRATICA 2
